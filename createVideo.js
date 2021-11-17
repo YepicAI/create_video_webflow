ccaddf4ddb5d663b06ed3d4cc88e323cf7a5a290
ioannisyepicai <60228522+ioannisyepicai@users.noreply.github.com>
2021-11-17 10:39:36 +0000

    Update createVideo.js

--- createVideo.js
+++ createVideo.js
@@ -694,44 +694,23 @@ $("#deleteBackground").on("click", function () {
   fV.voice = 0;
 });
 
+
+
 async function checkForAbuse() {
-  var text = fV.script;
-  //$("#video-script").val();
-  $.ajax({
-    url:"https://moderator-2xzgrl4rma-uc.a.run.app/text",
-    type: "POST",
-    data: JSON.stringify({text: text}),
-    processData: false,
-    headers: {
-      "Content-Type": "application/json",
-      "token": "575CDCE36ABB516771A658B055A61BAF657E1B8E",
-    },
-    success: function (data) {
-     if(data.sexual < 0.25 && data.mature < 0.25 && data.offensive < 0.25){
-      scriptApproved = true;
-      playPreview();     }
+  $.get({
+    url:
+      "https://us-central1-speech2vid-api.cloudfunctions.net/checkForAbuse?content=" +
+      fV.script,
+
+    success: function (approval) {
+      scriptApproved = approval;
+      playPreview();
     },
     error: function () {
       alert("Something went wrong, try again!");
     },
   });
-}
-
-//async function checkForAbuse() {
-//  $.get({
-//    url:
-//      "https://us-central1-speech2vid-api.cloudfunctions.net/checkForAbuse?content=" +
-//      fV.script,
-
-//    success: function (approval) {
-//      scriptApproved = approval;
-//      playPreview();
-//    },
-//    error: function () {
-//      alert("Something went wrong, try again!");
-//    },
-//  });
   
-//}
+}
 
 
