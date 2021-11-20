dba6351ce8396f571852e673d6b7729e6937524e
ioannisyepicai <60228522+ioannisyepicai@users.noreply.github.com>
2021-11-20 18:54:24 +0000

    Update createVideo.js

--- createVideo.js
+++ createVideo.js
@@ -529,7 +529,8 @@ async function uploadAudio() {
     },
     success: function (data) {
       fV.audioFilename = audioFileName;
-      fV.audioLink = data.mediaLink;
+      #fV.audioLink = data.mediaLink;
+      fv.audioLink = "gs://" + data.bucket + "/" + data.name
     },
     error: function () {
       alert("Something went wrong, try again!");
